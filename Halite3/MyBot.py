d0d1374601e16368b38a7ecc23ae1186284ebe7d
gabrielbessler <gabrielbessler@gmail.com>
2018-12-21 03:30:38 -0800

    Halite Bot Changes

--- Halite3/MyBot.py
+++ Halite3/MyBot.py
@@ -11,6 +11,7 @@ from hlt.positionals import Direction
 import random
 from random import shuffle
 import collections  
+from collections import defaultdict 
 
 # Can't use STDOUT - use logging
 import logging
@@ -21,8 +22,11 @@ game = hlt.Game()
 # Do computationally expensive start-up pre-processing here
 class Utils(object): 
     def __init__(self, DROPOFF_THRESHOLD=constants.MAX_HALITE * 0.8, MIN_HALITE=20, STOP_SPAWNING=165): 
+        '''
+        '''
         self.tiles_visited = set() 
         self.commands = []
+        self.targets = defaultdict(list) 
         self.me = None 
         self.game_map = None
         self.DROPOFF_THRESHOLD = DROPOFF_THRESHOLD
@@ -30,9 +34,19 @@ class Utils(object):
         self.STOP_SPAWNING = STOP_SPAWNING
 
     def tup(self, pos): 
+        '''
+        '''
+        pos = self.game_map.normalize(pos)
         return pos.x, pos.y
 
+    def detup(self, tup): 
+        '''
+        '''
+        return Position(*tup) 
+
     def checkShipSpawn(self, game): 
+        '''
+        '''
         # If the game is in the first 100 turns and you have enough halite, spawn a ship
         # Prevent ships from colliding on spawn
         if game.turn_number <= self.STOP_SPAWNING and self.me.halite_amount >= constants.SHIP_COST and not self.game_map[me.shipyard].is_occupied:
@@ -40,6 +54,8 @@ class Utils(object):
                 self.commands.append(me.shipyard.spawn())
 
     def update_game(self, me, game_map): 
+        '''
+        '''
         self.me = me 
         self.game_map = game_map 
 
@@ -47,6 +63,8 @@ class Utils(object):
         self.tiles_visited = set()
 
     def get_valid_position(self, vertex): 
+        '''
+        '''
         directions = Direction.get_all_cardinals()
         shuffle(directions) 
         possible_positions = [vertex.directional_offset(direction) for direction in directions]
@@ -55,14 +73,20 @@ class Utils(object):
                 return directions[index]
 
     def add_move_to_queue(self, ship, direction):
+        '''
+        '''
         self.commands.append(ship.move(direction))
         self.tiles_visited.add(self.tup(ship.position.directional_offset(direction)))
 
     def add_stationary_to_queue(self, ship): 
+        '''
+        '''
         self.commands.append(ship.stay_still())
         self.tiles_visited.add(self.tup(ship.position))
 
     def get_closest_dropoff(self, ship): 
+        '''
+        '''
         dropOffDistance = self.game_map.calculate_distance(self.me.shipyard.position, ship.position)
         dropOffIndex = -1
         for i in range(len(dropOffs)):
@@ -80,6 +104,8 @@ class Utils(object):
         return destination, dropOffDistance
 
     def check_stuck_ships(self): 
+        '''
+        '''
         # Check for ships that cannot move 
         ships_fixed = []
         for ship in self.me.get_ships(): 
@@ -90,6 +116,9 @@ class Utils(object):
         return ships_fixed
 
     def navigate(self, ship, destination, ignoreCollision = False): 
+        '''
+        '''
+
         directions = self.game_map.get_unsafe_moves(ship.position, drop_off_destination)
 
         direction = directions[0]
@@ -104,15 +133,45 @@ class Utils(object):
         elif self.tup(ship.position) not in utils.tiles_visited:
             utils.add_stationary_to_queue(ship)
         else: 
-            utils.add_move_to_queue(ship, utils.get_valid_position(ship.position))
+            direction = utils.get_valid_position(ship.position) 
+            if direction is not None: 
+                utils.add_move_to_queue(ship, direction)
+            else: 
+                utils.add_stationary_to_queue(ship)
+    
+    def A_star(self, start, goal): 
+        '''
+        '''
+
+        # Set of notes we have evaluated 
+        nodesVisited = set() 
+
+        # Set of currently discovered nodes that are not evaluated yet 
+        nodesDiscovered = set()
+        nodesDiscovered.add(start) 
+
+        # For each node, the node it can most efficiently be reached from 
+        cameFrom = {}
+
+        # Cost of going from start to that node 
+        gScore = {} 
+        gScore[start] = 0
+
+    def check_target(self, ship): 
+        '''
+        '''
+        if self.targets[ship.id] is None: 
+            return False  
+        self.navigate(ship, self.targets[ship.id])
+        return True 
 
 utils = Utils()
-MAX_STEPS_AHEAD = 25
-MIN_STEPS_AHEAD = 20
-game.ready("Steve v.1.0.2")
+MAX_STEPS_AHEAD = 60
+MIN_STEPS_AHEAD = 60
+game.ready("Steve v.1.0.4")
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#   Here, you log here your id, which you can always fetch from the game object by using my_id.
+#   Here, you log here your id, which you can alys fetch from the game object by using my_id.
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
             
 """ <<<Game Loop>>> """
@@ -129,6 +188,9 @@ while True:
     for ship in me.get_ships(): 
         if ship in ships_fixed:
             continue 
+        
+        if utils.check_target(ship):
+            continue
     
         # If the ship is full, return to the drop-off zone 
         # If the game is almost over, drop off the rest of our resources 
@@ -162,7 +224,7 @@ while True:
                 foundDirection = False 
                 steps_ahead = 0
 
-                visited = [] # Stores all of the vertices we have already looked at 
+                visited = set() # Stores all of the vertices we have already looked at 
                 queue = [[ship.position, [], 0]] # Each element is [position, path, cost]
                 max_score = 0
                 while (not foundDirection and steps_ahead < MAX_STEPS_AHEAD) or (steps_ahead < MIN_STEPS_AHEAD):        
@@ -171,11 +233,11 @@ while True:
                     # Check if this vertex is a good destinatiom
                     resources = game_map[vertex].halite_amount
                     if cost < resources and len(path) != 0 and utils.tup(ship.position.directional_offset(path[0])) not in utils.tiles_visited:
-                            score = (resources - cost)**2 / steps_ahead
-                            if score > max_score: 
-                                max_score = score
-                                directionToMove = path[0]
-                                foundDirection = True 
+                        score = (resources - cost)**2.5 / steps_ahead
+                        if score > max_score: 
+                            max_score = score
+                            directionToMove = path[0]
+                            foundDirection = True 
                     
                     # Get all tiles you can access from it 
                     directions = Direction.get_all_cardinals()
@@ -184,8 +246,9 @@ while True:
 
                     # For each tile, check if we have already visited it. If not, add to the queue 
                     for index, position in enumerate(possible_positions): 
-                        if position not in visited: 
-                            visited.append(position)
+                        pos = utils.tup(position)
+                        if pos not in visited: 
+                            visited.add(pos)
                             L = [position, path + [directions[index]], cost + (.1 * resources)] 
                             queue.append(L)
 
